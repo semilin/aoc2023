8129f09e0d4bd49b580c6223dca5c2fcb1cc46ce
semilin <semilinares@pm.me>
2022-12-01 17:35:12 -0800

    much better day 1 solution

--- day1/main.lisp
+++ day1/main.lisp
@@ -1,16 +1,13 @@
-(ql:quickload :alexandria)
 (ql:quickload :str)
-(let ((lines (str:lines (alexandria:read-file-into-string #P"input")))
-      (current-sum 0)
-      (totals nil))
-  (loop for line in lines
-	if (str:blank? line)
-	  do (progn (push current-sum totals)
-		    (setf current-sum 0))
-	else
-	  do (incf current-sum (parse-integer (str:trim line))))
-  (print (apply #'+ (let ((sorted (stable-sort totals #'>))) 
-		      (list (first sorted)
-			    (second sorted)
-			    (third sorted))))))
-
+(ql:quickload :split-sequence)
+(use-package :split-sequence)
+(ql:quickload :arrows)
+(use-package :arrows)
+(-<>> (uiop:read-file-string #P"input")
+      str:lines
+      (split-sequence-if #'str:blank?)
+      (mapcar (lambda (l)
+		(apply #'+ (mapcar #'parse-integer l))))
+      (sort <> #'>)
+      (subseq <> 0 3)
+      print)
